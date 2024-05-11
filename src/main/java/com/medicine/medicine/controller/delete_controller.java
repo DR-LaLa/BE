18202a9e9a9b7b8a337af7794b55da3209c9b65c
Hwangseoeun <sunny030921@gmail.com>
2024-05-11 15:10:43 +0900

    지워야 하는 파일들(초기 패키지 구조 설정을 위하여 세팅해 두었음)

---
+++ src/main/java/com/medicine/medicine/controller/delete_controller.java
@@ -0,0 +1,6 @@
+package com.medicine.medicine.controller;
+
+public class delete_controller {
+    //반드시 지우고 작업할 것
+    //초기 세팅을 위해 구성된 파일임
+}
---
+++ src/main/java/com/medicine/medicine/service/delete_service.java
@@ -0,0 +1,6 @@
+package com.medicine.medicine.service;
+
+public class delete_service {
+    //반드시 지우고 작업할 것
+    //초기 세팅을 위해 구성된 파일임
+}
