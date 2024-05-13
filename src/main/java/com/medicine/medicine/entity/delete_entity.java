d5e8726c596f0cbfe7ffb4aa99bddfd8a1c67f55
Hwangseoeun <sunny030921@gmail.com>
2024-05-14 00:42:02 +0900

    지워야 하는 파일들 지움(패키지 구조 설계하기 위함)

--- src/main/java/com/medicine/medicine/entity/delete_entity.java
+++
@@ -1,6 +0,0 @@
-package com.medicine.medicine.entity;
-
-public class delete_entity {
-    //반드시 지우고 작업할 것
-    //초기 세팅을 위해 구성된 파일임
-}
--- src/main/java/com/medicine/medicine/repository/delete_repository.java
+++
@@ -1,6 +0,0 @@
-package com.medicine.medicine.repository;
-
-public class delete_repository {
-    //반드시 지우고 작업할 것
-    //초기 세팅을 위해 구성된 파일임
-}
