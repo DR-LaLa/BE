900d75175a9d664afffb1d952ed9e20ba82b54c0
Hwangseoeun <129192366+Hwangseoeun@users.noreply.github.com>
2024-05-23 22:47:39 +0900

    Delete src/main/java/com/medicine/medicine/repository/UserRepository.java
    userrepository 파일의 중복으로 인해 commit 수정합니다!

--- src/main/java/com/medicine/medicine/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicine.medicine.repository;
-
-import com.medicine.medicine.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByLoginid(String loginid);
-}
-
