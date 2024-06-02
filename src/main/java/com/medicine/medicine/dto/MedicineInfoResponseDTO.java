8dd3a697c8c65781a5606ccdc0cd312049d6999b
Hwangseoeun <sunny030921@gmail.com>
2024-06-02 17:57:49 +0900

    의약 관련 open api 활용한 검색기능 - api 테스트 완료

---
+++ src/main/java/com/medicine/medicine/dto/MedicineInfoResponseDTO.java
@@ -0,0 +1,24 @@
+package com.medicine.medicine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MedicineInfoResponseDTO {
+
+    private String entpName;
+    private String itemName;
+    private String efcyQesitm;
+    private String useMethodQesitm;
+    private String atpnWarnQesitm;
+    private String atpnQesitm;
+    private String intrcQesitm;
+    private String seQesitm;
+    private String depositMethodQesitm;
+
+}
